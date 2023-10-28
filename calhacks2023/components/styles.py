88567297fffaa517ada97238da6492a0b8413ee5
Etienne Devictor <etiennedevictor@Etiennes-MacBook-Air.local>
2023-10-27 21:56:01 -0700

    moved styles outside of textbox.py

---
+++ calhacks2023/components/styles.py
@@ -0,0 +1,18 @@
+action_style = {
+    "width": "800px;",
+}
+chat_style = {
+    # "display": "flex",
+    # "flex-direction": "row",
+    # "justify-content": "space-between",
+    "width": "100%",
+    "question": {
+        "max-width": "75%",
+        "color": "red;",
+    },
+    "answer": {
+        "max-width": "75%",
+        "color": "blue"
+
+    }
+}
--- calhacks2023/components/textbox.py
+++ calhacks2023/components/textbox.py
@@ -1,26 +1,15 @@
 import reflex as rx
 from calhacks2023.state import State
-
-box_style = {
-    "width": "800px;",
-}
-input_style = {
-    "height": "200px;",
-    "width:": "350px",
-    "text-align": "left;",
-}
+from .styles import *
 
 
 class FormState(State):
 
     # The current question being asked.
     question: str
-    form_data: dict = {}
 
     # Keep track of the chat history as a list of (question, answer) tuples.
     chat_history: list[tuple[str, str]] = []
-    chat_history.append(("what is a person", "bro I dont know"))
-    chat_history.append(("what is a person", "bro I dont know"))
 
     def set_question(self, input):
         self.question = input
@@ -41,11 +30,13 @@ def qa(question, answer) -> rx.Component:
             question,
             # The user's question is on the right.
             text_align="right",
+            style=chat_style.get("question"),
         ),
         rx.box(
             answer,
             # The answer is on the left.
             text_align="left",
+            style=chat_style.get("answer"),
         ),
     )
 
@@ -55,7 +46,8 @@ def chat() -> rx.Component:
         rx.foreach(
             FormState.chat_history,
             lambda messages: qa(messages[0], messages[1]),
-        )
+        ),
+        style=chat_style,
     )
 
 
@@ -71,6 +63,7 @@ def action_bar() -> rx.Component:
             on_click=FormState.answer,
             # style=style.button_style,
         ),
+        style=action_style,
     )
 
 

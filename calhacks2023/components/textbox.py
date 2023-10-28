287e357a059598cdaacea398b1e85de844f11d17
Pablo Nava <pablonavab@gmail.com>
2023-10-28 02:22:23 -0700

    changes

--- calhacks2023/components/textbox.py
+++ calhacks2023/components/textbox.py
@@ -8,9 +8,10 @@ class FormState(State):
 
     # The current question being asked.
     question: str
+    default_art_style = "cartoon"
     default_user_message = "I just spawned in"
     default_model_message = "You are lost in a forest, with no tools or weapons. You notice an old box near you, but are unsure whether you should open it..."
-    default_image_code = get_ai_image(default_model_message)
+    default_image_code = get_ai_image(default_model_message, default_art_style)
 
     # Keep track of the chat history as a list of (question, answer) tuples.
     chat_history: list[dict[str, str]] = [{"user": default_user_message, "model": default_model_message, "image_code": default_image_code}]
@@ -27,7 +28,7 @@ class FormState(State):
         self.chat_history.append({"user": self.question, "model": "loading", "image_code": ""})
         ai_answer = get_ai_response(self.truncate_chat_history(), self.question)
         print(f"answer is {ai_answer}")
-        image_code = get_ai_image(ai_answer)
+        image_code = get_ai_image(ai_answer, self.default_art_style)
         self.chat_history[-1]["model"] = ai_answer
         self.chat_history[-1]["image_code"] = image_code
 

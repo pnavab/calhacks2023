1738582217a5856184c642d0cf9eec04d3534058
bobo <danknessdra@gmail.com>
2023-10-27 18:46:14 -0700

    backend

--- calhacks2023/calhacks2023.py
+++ calhacks2023/calhacks2023.py
@@ -4,9 +4,13 @@ from calhacks2023 import styles
 
 # Import all the pages.
 from calhacks2023.pages import *
-
+from calhacks2023.backend.ai import *
 import reflex as rx
 
 # Create the app and compile it.
 app = rx.App(style=styles.base_style)
+app.api.add_api_route(
+    path = "/",
+    endpoint = root
+)
 app.compile()
